7ba75e5ad757ac0df7ece0be613c7429f361d0bf
shaivi123 <shaivi@weetechsolution.com>
2022-10-05 17:49:39 +0530

    added a text file

---
+++ src/main/java/com/springrest/GitLearning/GitLearningApplication.java
@@ -0,0 +1,16 @@
+package com.springrest.GitLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLearningApplication.class, args);
+		System.out.println("hii");
+
+
+	}
+
+}
